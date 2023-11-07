3b9e0be355fb704874763df06f82389c8a6d9e7b
Deilon <deilast80@gmail.com>
2023-11-07 21:20:43 +0800

    Rename to Sales Revenue Report

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -27,7 +27,7 @@
         <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-layout"></i>
-                <div data-i18n="Layouts">Sales Record</div>
+                <div data-i18n="Layouts">Sales Revenue Report</div>
                 {{-- <div data-i18n="Layouts">Layouts</div> --}}
             </a>
 
