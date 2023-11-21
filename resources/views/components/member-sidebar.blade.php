9a1bf1c19fdb0122bb3b210ca3a652d7590d4398
Deilon <deilast80@gmail.com>
2023-11-22 03:50:32 +0800

    Dashboard logo link

--- resources/views/components/member-sidebar.blade.php
+++ resources/views/components/member-sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Menu -->
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="index.html" class="app-brand-link mx-auto">
+        <a href="{{ url(Auth::user()->role.'/dashboard') }}" class="app-brand-link mx-auto">
             <span class="app-brand-logo demo">
                 <img src="{{ asset('storage/assets/img/logo/logo.svg') }}" alt="triangle with all three sides equal" height="auto" width="130" />
             </span>
