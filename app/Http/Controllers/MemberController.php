92a9a0d7906e69457b2002ab799613f32e1549b1
Deilon <deilast80@gmail.com>
2023-11-23 04:17:59 +0800

    Minimize code for trainer details on members

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -257,75 +257,19 @@ class MemberController extends Controller
      * Trainer Progress View
      */
     public function trainerProgress() {
-
         $subscription = Subscription::where('user_id', Auth::user()->id)->first();
-        $data['subscription'] = $subscription;
-        if($subscription) {
-            $current_date = date('Y-m-d');
-            $start_timestamp = strtotime($subscription->start_date);
-            $end_timestamp = strtotime($subscription->end_date);
-            $current_timestamp = strtotime($current_date);
-            $data['total_days'] = ($end_timestamp - $start_timestamp) / (60 * 60 * 24);
-            $data['days_elapsed'] = ($current_timestamp - $start_timestamp) / (60 * 60 * 24);
-            $data['percentage_completed'] = ($data['days_elapsed'] / $data['total_days']) * 100;
-    
-            $data['start_date'] = Carbon::parse($subscription->start_date);
-            $data['end_date']= Carbon::parse($subscription->end_date);
-    
-            
-            $data['tier'] = SubscriptionTier::where('id', $subscription->subscription_tier_id)->first();
-            $data['user'] = Auth::user();
-
-            $data['assigned_staff'] = User::find($subscription->staff_assigned_id);
-
-            if($subscription->payment_option == 'credit card') {
-                $data['creditCard'] = CreditCard::where('subscription_id', $subscription->id)->first();
-            } else if ($subscription->payment_option == 'gcash') {
-                $data['gCash'] = Gcash::where('subscription_id', $subscription->id)->first();
-            } else if ($subscription->payment_option == 'manual payment') {
-                $data['manualPayment'] = ManualPayment::where('subscription_id', $subscription->id)->first();
-            }
-        }
+        $data['user'] = Auth::user();
+        $data['assigned_staff'] = User::find($subscription->staff_assigned_id);
 
         // $trainerSubscriptions = Subscription::where('staff_assigned_id', $memberSubscription->staff_assigned_id)->get();
         $data['progressWeeks'] = ProgressWeek::where('user_id', $subscription->staff_assigned_id)->get();
-        
-        
-        $data['user'] = Auth::user();
         return view('member.trainer-progress', $data);
     }
 
     public function staticTrainerWeek($wkProgressId) {
-
         $subscription = Subscription::where('user_id', Auth::user()->id)->first();
-        $data['subscription'] = $subscription;
-        if($subscription) {
-            $current_date = date('Y-m-d');
-            $start_timestamp = strtotime($subscription->start_date);
-            $end_timestamp = strtotime($subscription->end_date);
-            $current_timestamp = strtotime($current_date);
-            $data['total_days'] = ($end_timestamp - $start_timestamp) / (60 * 60 * 24);
-            $data['days_elapsed'] = ($current_timestamp - $start_timestamp) / (60 * 60 * 24);
-            $data['percentage_completed'] = ($data['days_elapsed'] / $data['total_days']) * 100;
-    
-            $data['start_date'] = Carbon::parse($subscription->start_date);
-            $data['end_date']= Carbon::parse($subscription->end_date);
-    
-            
-            $data['tier'] = SubscriptionTier::where('id', $subscription->subscription_tier_id)->first();
-            $data['user'] = Auth::user();
-
-            $data['assigned_staff'] = User::find($subscription->staff_assigned_id);
-
-            if($subscription->payment_option == 'credit card') {
-                $data['creditCard'] = CreditCard::where('subscription_id', $subscription->id)->first();
-            } else if ($subscription->payment_option == 'gcash') {
-                $data['gCash'] = Gcash::where('subscription_id', $subscription->id)->first();
-            } else if ($subscription->payment_option == 'manual payment') {
-                $data['manualPayment'] = ManualPayment::where('subscription_id', $subscription->id)->first();
-            }
-        }
-
+        $data['user'] = Auth::user();
+        $data['assigned_staff'] = User::find($subscription->staff_assigned_id);
 
         $data['weekProgress'] = ProgressWeek::find($wkProgressId);
         $data['days'] = ProgressDay::where('progress_week_id', $wkProgressId)->orderBy('day_number', 'asc')->get();
